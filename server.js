471dc9a1fcfe5046599a9d2a6c072cd9405ba2b4
garyb23uk <garyb23uk@gmail.com>
2019-02-13 21:11:20 +0000

    Include static path and default route

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 const express = require('express');
-const app = express();
+const path = require('path');
 const port = 3000;
+const app = express();
 
-app.get('/', (req, res) => res.send('Express Starter'))
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname + '/index.html'));
+});
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
